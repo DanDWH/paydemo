e2b03895b651ac879da62d486434f15a75233888
dan_hao <dengwh@asiainfo.com>
2019-10-28 15:41:25 +0800

    dwh first try

---
+++ src/test/java/com/dwh/paydemo/PaydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwh.paydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
